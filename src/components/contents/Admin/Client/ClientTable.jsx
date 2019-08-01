3585f7f1d1cba956e23ec731b01ea20f465e249c
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-08-01 10:49:01 +0200

    Change: table export

--- src/components/contents/Admin/Client/ClientTable.jsx
+++ src/components/contents/Admin/Client/ClientTable.jsx
@@ -29,70 +29,6 @@ const styles = theme => ({
 function LoanTable(props) {
   const { classes, data } = props;
   return (
-<<<<<<< HEAD
-    <Paper className={classes.root}>
-      <div className={classes.table}>
-        <div className="table-header">
-          <div className="header header-name">
-            CLIENTE
-          </div>
-          <div className="header header-company">
-            COMPAÑÍA
-          </div>
-          <div className="header header-loan-number">
-            #
-          </div>
-          <div className="header header-capital">
-            PRESTAMO
-          </div>
-          <div className="header header-remaining">
-            X REPAGAR
-          </div>
-          <div className="header header-payment">
-            CUOTA
-          </div>
-          <div className="header header-date">
-            FECHA
-          </div>
-        </div>
-        <div className='table-body'>
-          {data.length > 0 ? data.map((row, i) => {
-            return (
-                <div className="ser-item-holder">
-                  <div className="ser-name-country">
-                    <CountryFlag country={row.country} />
-                      <h4 className="ser-client">
-                        <Link to={`/admin/client/${row._id}`}>{row.firstName + " " + row.lastName}</Link>
-                      </h4>
-                    </div>
-                    <div className="ser-businessName">
-                      <p>{row.businessName ? row.businessName : ""}</p>
-                    </div>
-                    <div className="ser-loanAmount">
-                      <p>{_(row.loans).groupBy('_id').size()}</p>
-                    </div>
-                    <div className="ser-loan">
-                      {row.loans.map(e => {
-                        return (
-                          <div className="ser-loan-details">
-                            <p className="ser-loan-detail loan-amount"><Link to={`/admin/loan/${e._id}`}>{(e.capital).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}</Link></p>
-                            <p className="ser-loan-detail">{(e.capitalRemaining).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                            <p className="ser-loan-detail">{(e.nextPayment.interest + e.nextPayment.principal).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                            <p className="ser-loan-detail">{moment(e.nextPayment.date).format('YYYY-MM-DD')}</p>
-                          </div>
-                        )
-                      })}
-                  </div>
-                </div>
-            ) 
-          }) : ""}    
-        </div>
-        </div>
-    </Paper>
-  );  
-}   
- 
-=======
     <>
       <ReactHTMLTableToExcel
         id="test-table-xls-button"
@@ -216,9 +152,8 @@ function LoanTable(props) {
   );
 }
 
->>>>>>> feature-excel-output
 LoanTable.propTypes = {
   classes: PropTypes.object.isRequired,
 };
- 
+
 export default withStyles(styles)(LoanTable);
