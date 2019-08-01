909c04cd9bbf63656675975b9d8cbf80ab6ed331
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-08-01 09:06:14 +0200

    New: excel export intro

--- src/components/contents/Admin/Client/ClientTable.jsx
+++ src/components/contents/Admin/Client/ClientTable.jsx
@@ -8,7 +8,7 @@ import _ from 'lodash'
 import { Link } from 'react-router-dom'
 import './results-table.scss'
 import CountryFlag from '../../../helpers/CountryFlag.jsx'
-
+import ReactHTMLTableToExcel from 'react-html-table-to-excel';
 
 const styles = theme => ({
   root: {
@@ -31,67 +31,73 @@ function LoanTable(props) {
   const { classes, data } = props;
   return (
     <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <div className="table-header">
-          <div className="header header-name">
+      <ReactHTMLTableToExcel
+        id="test-table-xls-button"
+        className="download-table-xls-button"
+        table="table-to-xls"
+        filename="tablexls"
+        sheet="tablexls"
+        buttonText="Download as XLS" />
+      <table className={classes.table} id="table-to-xls">
+        <thead className="table-header">
+          <th className="header header-name">
             CLIENTE
-          </div>
-          <div className="header header-company">
+          </th>
+          <th className="header header-company">
             COMPAÑÍA
-          </div>
-          <div className="header header-loan-number">
+          </th>
+          <th className="header header-loan-number">
             #
-          </div>
-          <div className="header header-capital">
+          </th>
+          <th className="header header-capital">
             PRESTAMO
-          </div>
-          <div className="header header-remaining">
+          </th>
+          <th className="header header-remaining">
             X REPAGAR
-          </div>
-          <div className="header header-payment">
+          </th>
+          <th className="header header-payment">
             CUOTA
-          </div>
-          <div className="header header-date">
+          </th>
+          <th className="header header-date">
             FECHA
-          </div>
-        </div>
-        <div className='table-body'>
+          </th>
+        </thead>
+        <tbody className='table-body'>
           {data.length > 0 ? data.map((row, i) => {
             console.log([_.countBy(row.loans, '_id')].length)
             return (
-              <>
-                <div className="ser-item-holder">
-                  <div className="ser-name-country">
-                    <CountryFlag country={row.country} />
-                    <h4 className="ser-client">
-                      <Link to={`/admin/client/${row._id}`}>{row.firstName + " " + row.lastName}</Link>
-                    </h4>
-                  </div>
-                  <div className="ser-businessName">
-                    <p>{row.businessName ? row.businessName : ""}</p>
-                  </div>
-                  <div className="ser-loanAmount">
-                    <p>{_(row.loans).groupBy('_id').size()}</p>
-                  </div>
-                  <div className="ser-loan">
-                    {row.loans.map(e => {
-                      return (
-                        <div className="ser-loan-details">
-                          <p className="ser-loan-detail loan-amount"><Link to={`/admin/loan/${e._id}`}>{(e.capital).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}</Link></p>
-                          <p className="ser-loan-detail">{(e.capitalRemaining).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                          <p className="ser-loan-detail">{(e.nextPayment.interest + e.nextPayment.principal).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                          <p className="ser-loan-detail">{moment(e.nextPayment.date).format('YYYY-MM-DD')}</p>
-                        </div>
-                      )
-                    })}
-                  </div>
 
-                </div>
-              </>
+              <tr className="ser-item-holder">
+                <td rowspan={_(row.loans).groupBy('_id').size()} className="ser-name-country">
+                  <CountryFlag country={row.country} />
+                  <h4 className="ser-client">
+                    <Link to={`/admin/client/${row._id}`}>{row.firstName + " " + row.lastName}</Link>
+                  </h4>
+                </td>
+                <td rowspan={_(row.loans).groupBy('_id').size()} className="ser-businessName">
+                  <p>{row.businessName ? row.businessName : ""}</p>
+                </td>
+                <td rowspan={_(row.loans).groupBy('_id').size()} className="ser-loanAmount">
+                  <p>{_(row.loans).groupBy('_id').size()}</p>
+                </td>
+                <tr className="ser-loan">
+                  {row.loans.map(e => {
+                    return (
+                      <td colspan='4' className="ser-loan-details">
+                        <tr className="ser-loan-detail loan-amount"><Link to={`/admin/loan/${e._id}`}>{(e.capital).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}</Link></tr>
+                        <tr className="ser-loan-detail">{(e.capitalRemaining).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</tr>
+                        <tr className="ser-loan-detail">{(e.nextPayment.interest + e.nextPayment.principal).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</tr>
+                        <tr className="ser-loan-detail">{moment(e.nextPayment.date).format('YYYY-MM-DD')}</tr>
+                      </td>
+                    )
+                  })}
+                </tr>
+              </tr>
+
             )
           }) : ""}
-        </div>
-      </Table>
+        </tbody>
+      </table>
     </Paper>
   );
 }
