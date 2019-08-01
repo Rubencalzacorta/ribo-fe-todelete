3866f9e1014611b383ee80dfead7413e7a0dbef0
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-08-01 09:31:36 +0200

    Hotfix: minor changes to result table

--- src/components/contents/Admin/Client/ClientTable.jsx
+++ src/components/contents/Admin/Client/ClientTable.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
 import Paper from '@material-ui/core/Paper';
 import moment from 'moment'
 import _ from 'lodash'
@@ -31,7 +30,7 @@ function LoanTable(props) {
   const { classes, data } = props;
   return (
     <Paper className={classes.root}>
-      <Table className={classes.table}>
+      <div className={classes.table}>
         <div className="table-header">
           <div className="header header-name">
             CLIENTE
@@ -57,47 +56,43 @@ function LoanTable(props) {
         </div>
         <div className='table-body'>
           {data.length > 0 ? data.map((row, i) => {
-            console.log([_.countBy(row.loans, '_id')].length)
             return (
-              <>
                 <div className="ser-item-holder">
                   <div className="ser-name-country">
                     <CountryFlag country={row.country} />
-                    <h4 className="ser-client">
-                      <Link to={`/admin/client/${row._id}`}>{row.firstName + " " + row.lastName}</Link>
-                    </h4>
+                      <h4 className="ser-client">
+                        <Link to={`/admin/client/${row._id}`}>{row.firstName + " " + row.lastName}</Link>
+                      </h4>
+                    </div>
+                    <div className="ser-businessName">
+                      <p>{row.businessName ? row.businessName : ""}</p>
+                    </div>
+                    <div className="ser-loanAmount">
+                      <p>{_(row.loans).groupBy('_id').size()}</p>
+                    </div>
+                    <div className="ser-loan">
+                      {row.loans.map(e => {
+                        return (
+                          <div className="ser-loan-details">
+                            <p className="ser-loan-detail loan-amount"><Link to={`/admin/loan/${e._id}`}>{(e.capital).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}</Link></p>
+                            <p className="ser-loan-detail">{(e.capitalRemaining).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
+                            <p className="ser-loan-detail">{(e.nextPayment.interest + e.nextPayment.principal).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
+                            <p className="ser-loan-detail">{moment(e.nextPayment.date).format('YYYY-MM-DD')}</p>
+                          </div>
+                        )
+                      })}
                   </div>
-                  <div className="ser-businessName">
-                    <p>{row.businessName ? row.businessName : ""}</p>
-                  </div>
-                  <div className="ser-loanAmount">
-                    <p>{_(row.loans).groupBy('_id').size()}</p>
-                  </div>
-                  <div className="ser-loan">
-                    {row.loans.map(e => {
-                      return (
-                        <div className="ser-loan-details">
-                          <p className="ser-loan-detail loan-amount"><Link to={`/admin/loan/${e._id}`}>{(e.capital).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}</Link></p>
-                          <p className="ser-loan-detail">{(e.capitalRemaining).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                          <p className="ser-loan-detail">{(e.nextPayment.interest + e.nextPayment.principal).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-                          <p className="ser-loan-detail">{moment(e.nextPayment.date).format('YYYY-MM-DD')}</p>
-                        </div>
-                      )
-                    })}
-                  </div>
-
                 </div>
-              </>
-            )
-          }) : ""}
+            ) 
+          }) : ""}    
         </div>
-      </Table>
+        </div>
     </Paper>
-  );
-}
-
+  );  
+}   
+ 
 LoanTable.propTypes = {
   classes: PropTypes.object.isRequired,
 };
-
+ 
 export default withStyles(styles)(LoanTable);
