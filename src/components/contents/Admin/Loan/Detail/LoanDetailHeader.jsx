379c7e410aa9ecfb1c9ef2bb719e942e7c4bb4d1
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-08-01 17:02:36 +0200

    New: it now displays IRR and Payback Period

--- src/components/contents/Admin/Loan/Detail/LoanDetailHeader.jsx
+++ src/components/contents/Admin/Loan/Detail/LoanDetailHeader.jsx
@@ -6,38 +6,59 @@ import './loanDetails.scss'
 function LoanDetailHeader(props) {
     const { details, interestIncome, interestProjected } = props;
     return (
-        
+
         <div className="one-loan-details-summary">
             <div className="loan-detail-summary loan-detail-left">
-                <p className='title'><Link className='link' to={`/admin/client/${details._borrower._id}`}>{details._borrower.firstName.toUpperCase()+" "+details._borrower.lastName.toUpperCase()}</Link></p>
-                <p className='total'>${parseFloat(details.capital).toLocaleString(undefined, { maximumFractionDigits: 2 })}
-                    <span className='acc-total'>/ {moment(details.startDate).format('LL')}</span>
+                <p className='title name'><Link className='link' to={`/admin/client/${details._borrower._id}`}>{details._borrower.firstName.toUpperCase() + " " + details._borrower.lastName.toUpperCase()}</Link></p>
+                <p className='total'>${parseFloat(details.capital).toLocaleString(undefined, { maximumFractionDigits: 2 }) + " " + details.currency}
                 </p>
             </div>
             <div className="loan-detail-summary loan-detail-content">
                 <div>
-                    <p className='title'>DETALLE</p>
-                    <p className='total'>{parseFloat(details.interest)}% | {details.duration} m | {details.currency}</p>
+                    <p className='title'>INT & TME</p>
+                    <p className='total'>{parseFloat(details.interest)}% | {parseFloat(details.IRR)}% </p>
                 </div>
             </div>
             <div className="loan-detail-summary loan-detail-content">
                 <div>
                     <p className='title'>POR AMORTIZAR</p>
                     <p className='total'>${((details.capitalRemaining > 1 ? details.capitalRemaining : 0)).toLocaleString(undefined, { maximumFractionDigits: 2 })}
-                        <span className='acc-total'>/${(isNaN(details.totalPaid)) ? 0 : parseFloat(details.totalPaid).toLocaleString(undefined, { maximumFractionDigits: 2 })}</span>
                     </p>
                 </div>
             </div>
             <div className="loan-detail-summary last loan-detail-content">
                 <div>
-                    <p className='title'>INTERESES</p>
-                    <p className='total'>${(interestIncome).toLocaleString(undefined, { maximumFractionDigits: 2 })}
-                        <span className='acc-total'>/${interestProjected.toLocaleString(undefined, { maximumFractionDigits: 2 })}</span>
+                    <p className='title'>INGRESO</p>
+                    <p className='total'>${(interestIncome).toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                </div>
+            </div>
+            <div className="loan-detail-summary loan-detail-left">
+                <p className='title'>INICIO</p>
+                <p className='total'>
+                    <span className='total'>{moment(details.startDate).format('LL')}</span>
+                </p>
+            </div>
+            <div className="loan-detail-summary loan-detail-content">
+                <div>
+                    <p className='title'>PLAZO & PR</p>
+                    <p className='total'>{details.duration} m | {(details.PaybackPeriod).toLocaleString(undefined, { maximumFractionDigits: 2 })} m</p>
+                </div>
+            </div>
+            <div className="loan-detail-summary loan-detail-content">
+                <div>
+                    <p className='title'>AMORTIZADO</p>
+                    <p className='total'>${(isNaN(details.totalPaid)) ? 0 : parseFloat(details.totalPaid).toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                </div>
+            </div>
+            <div className="loan-detail-summary last loan-detail-content">
+                <div>
+                    <p className='title'>INTERES PROJECTADO</p>
+                    <p className='total'>${interestProjected.toLocaleString(undefined, { maximumFractionDigits: 2 })}
                     </p>
                 </div>
             </div>
         </div>
-            );
+    );
 }
 
 
