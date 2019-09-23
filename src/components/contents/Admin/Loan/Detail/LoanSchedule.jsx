98dc456a250b43abb840acccacbbb6654994ea7f
Miguel Gomez Barnola <mgomezb89@gmail.com>
2019-09-23 17:59:35 +0200

    New: added payments to schedule

--- src/components/contents/Admin/Loan/Detail/LoanSchedule.jsx
+++ src/components/contents/Admin/Loan/Detail/LoanSchedule.jsx
@@ -41,41 +41,82 @@ function LoanSchedule(props) {
             </div>
             {loanSchedule.map((item, i) => {
                 return (
-                    <div key={i} className="loan-schedule-content">
-                        <div className="detail-schedule details-date">
-                            <p className='acc-date'>{moment(item.date).format("YY-MM-DD")}</p>
+                    <>
+                        <div key={i} className="loan-schedule-content">
+                            <div className="detail-schedule details-date">
+                                <p className='acc-date'>{moment(item.date).format("YY-MM-DD")}</p>
+                            </div>
+                            <div className="detail-schedule details-content">
+                                <p className='acc-total'>{item.status}</p>
+                            </div>
+                            <div className="detail-schedule details-content">
+                                <p className='acc-total'>{item.interest.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                            </div>
+                            <div className="detail-schedule details-content">
+                                <p className='acc-total'>{item.principal.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                            </div>
+                            <div className="detail-schedule details-content">
+                                <p className='acc-total'>{(item.interest + item.principal).toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                            </div>
+                            <div className="detail-schedule details-content">
+                                <p className='acc-total'>{(item.interest_pmt) ? item.interest_pmt.toLocaleString(undefined, { maximumFractionDigits: 2 }) : ""}</p>
+                            </div>
+                            <div className="detail-schedule details-content">
+                                <p className='acc-total'>{(item.principal_pmt) ? item.principal_pmt.toLocaleString(undefined, { maximumFractionDigits: 2 }) : ""}</p>
+                            </div>
+                            <div className="detail-schedule details-content">
+                                <p className='acc-total'>{(item.principal_pmt || item.interest_pmt) ? ((item.principal_pmt ? item.principal_pmt : 0) + (item.interest_pmt ? item.interest_pmt : 0)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ""}</p>
+                            </div>
+                            <div className="detail-schedule details-content">
+                                <p className='acc-total'>{(item.cashAccount) ? item.cashAccount : ""}</p>
+                            </div>
+                            <div className="detail-schedule last details-content">
+                                {(item.interest_pmt || item.principal_pmt || (i === 0)) ?
+                                    (i < 1) ? "" : <p className='itmt-options'><i className="material-icons" onClick={() => reversePayment(item._id)}> delete_box </i></p>
+                                    :
+                                    <p className='itmt-options'><i className="material-icons" onClick={() => openPaymentOption(item)}> add_box </i></p>}
+                            </div>
                         </div>
-                        <div className="detail-schedule details-content">
-                            <p className='acc-total'>{item.status}</p>
-                        </div>
-                        <div className="detail-schedule details-content">
-                            <p className='acc-total'>{item.interest.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
-                        </div>
-                        <div className="detail-schedule details-content">
-                            <p className='acc-total'>{item.principal.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
-                        </div>
-                        <div className="detail-schedule details-content">
-                            <p className='acc-total'>{(item.interest + item.principal).toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
-                        </div>
-                        <div className="detail-schedule details-content">
-                            <p className='acc-total'>{(item.interest_pmt) ? item.interest_pmt.toLocaleString(undefined, { maximumFractionDigits: 2 }) : ""}</p>
-                        </div>
-                        <div className="detail-schedule details-content">
-                            <p className='acc-total'>{(item.principal_pmt) ? item.principal_pmt.toLocaleString(undefined, { maximumFractionDigits: 2 }) : ""}</p>
-                        </div>
-                        <div className="detail-schedule details-content">
-                            <p className='acc-total'>{(item.principal_pmt || item.interest_pmt) ? ((item.principal_pmt ? item.principal_pmt : 0) + (item.interest_pmt ? item.interest_pmt : 0)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ""}</p>
-                        </div>
-                        <div className="detail-schedule details-content">
-                            <p className='acc-total'>{(item.cashAccount) ? item.cashAccount : ""}</p>
-                        </div>
-                        <div className="detail-schedule last details-content">
-                            {(item.interest_pmt || item.principal_pmt || (i === 0)) ?
-                                (i < 1) ? "" : <p className='itmt-options'><i className="material-icons" onClick={() => reversePayment(item._id)}> delete_box </i></p>
-                                :
-                                <p className='itmt-options'><i className="material-icons" onClick={() => openPaymentOption(item)}> add_box </i></p>}
-                        </div>
-                    </div>
+                        {item.payments.length > 0 ? item.payments.map((e) => {
+                            return (
+                                <div key={e._id} className="loan-schedule-content">
+                                    <div className="detail-schedule details-date">
+                                        <p className='acc-date'>{moment(e.date_pmt).format("YY-MM-DD")}</p>
+                                    </div>
+                                    <div className="detail-schedule details-content">
+                                        <p className='acc-total'>PAYMENT</p>
+                                    </div>
+                                    <div className="detail-schedule details-content">
+                                        <p className='acc-total'></p>
+                                    </div>
+                                    <div className="detail-schedule details-content">
+                                        <p className='acc-total'></p>
+                                    </div>
+                                    <div className="detail-schedule details-content">
+                                        <p className='acc-total'></p>
+                                    </div>
+                                    <div className="detail-schedule details-content">
+                                        <p className='acc-total'></p>
+                                    </div>
+                                    <div className="detail-schedule details-content">
+                                        <p className='acc-total'></p>
+                                    </div>
+                                    <div className="detail-schedule details-content">
+                                        <p className='acc-total'>{e.amount}</p>
+                                    </div>
+                                    <div className="detail-schedule details-content">
+                                        <p className='acc-total'>{(e.cashAccount) ? e.cashAccount : ""}</p>
+                                    </div>
+                                    <div className="detail-schedule last details-content">
+                                        {(item.interest_pmt || item.principal_pmt || (i === 0)) ?
+                                            (i < 1) ? "" : <p className='itmt-options'><i className="material-icons" onClick={() => reversePayment(item._id)}> delete_box </i></p>
+                                            :
+                                            <p className='itmt-options'><i className="material-icons" onClick={() => openPaymentOption(item)}> add_box </i></p>}
+                                    </div>
+                                </div>
+                            )
+                        }) : ""}
+                    </>
                 )
             })}
         </div>
