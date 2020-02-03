46fcd8fc5be12a521acecf546b1d89b93b3c1f3a
Miguel Gomez Barnola <mgomezb89@gmail.com>
2020-02-03 10:44:22 +0100

    Switched admin location from WORLD to GLOBAL

--- src/components/contents/Admin/Loan/PeriodSchedule.jsx
+++ src/components/contents/Admin/Loan/PeriodSchedule.jsx
@@ -28,10 +28,10 @@ export default class PeriodList extends Component {
   }
 
   fetchLoans(e) {
-      if (e) {e.preventDefault(e)}
-      const {startDate, endDate} = this.state
-      const { userLocation } = this.props
-      this.service.getPeriodSchedule(startDate, endDate, userLocation)
+    if (e) { e.preventDefault(e) }
+    const { startDate, endDate } = this.state
+    const { userLocation } = this.props
+    this.service.getPeriodSchedule(startDate, endDate, userLocation)
       .then(response => {
         this.setState({
           getLoan: false,
@@ -40,42 +40,42 @@ export default class PeriodList extends Component {
 
       })
       .catch(err => {
-          this.setState({
-            loans: false
+        this.setState({
+          loans: false
         })
       })
   }
 
-  
-  
+
+
 
   render() {
     const { country, loans } = this.state;
-    
-    const filteredLoans = loans.filter(e => ((country !== 'Todos') ? e.borrower[0].country.includes(country.toUpperCase()) : e)) 
+
+    const filteredLoans = loans.filter(e => ((country !== 'Todos') ? e.borrower[0].country.includes(country.toUpperCase()) : e))
 
     return (
       <div className="content">
-      <div className="form-row input-row-aligner">
-        <div className="form-group col-md-2">
-          <label>Fecha Inicial</label>
-          <input type="date" className="form-control" id="startDate" name="startDate" value={this.state.startDate} onChange={e => this.handleChange(e)} required />
+        <div className="form-row input-row-aligner">
+          <div className="form-group col-md-2">
+            <label>Fecha Inicial</label>
+            <input type="date" className="form-control" id="startDate" name="startDate" value={this.state.startDate} onChange={e => this.handleChange(e)} required />
+          </div>
+          <div className="form-group col-md-2">
+            <label>Fecha Final</label>
+            <input type="date" className="form-control" id="endDate" name="endDate" value={this.state.endDate} onChange={e => this.handleChange(e)} required />
+          </div>
+          <div className="form-group col-md-1 ">
+            <button className="btn btn-info" type="button" onClick={e => this.fetchLoans(e)}>Buscar</button>
+          </div>
+          {(this.props.userLocation === "GLOBAL") ?
+            <div className="form-group col-md-2 ">
+              <label>País</label>
+              <select className="form-control" name="country" id="country" value={this.state.country} onChange={e => this.handleChange(e)}>
+                {(this.state.countries) ? this.state.countries.map((e, i) => <option key={i} value={e}>{e}</option>) : ""}
+              </select>
+            </div> : ""}
         </div>
-        <div className="form-group col-md-2">
-          <label>Fecha Final</label>
-          <input type="date" className="form-control" id="endDate" name="endDate" value={this.state.endDate} onChange={e => this.handleChange(e)} required />
-        </div>
-        <div className="form-group col-md-1 ">
-        <button className="btn btn-info" type="button" onClick={e => this.fetchLoans(e)}>Buscar</button>
-        </div>
-        {( this.props.userLocation === "WORLD") ?
-        <div className="form-group col-md-2 ">
-        <label>País</label>
-        <select className="form-control" name="country" id="country" value={this.state.country} onChange={e => this.handleChange(e)}>
-          {(this.state.countries) ? this.state.countries.map( (e, i) => <option key={i} value={e}>{e}</option>) : ""}
-        </select>
-        </div>:""}
-      </div>
         <WeeklyScheduleTable data={filteredLoans} />
       </div>
     )
